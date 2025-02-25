acec65bf02497bdf0671cc484dfca7395e38694a
furkanturan8 <turanfurkan830@gmail.com>
2025-02-25 13:49:04 +0300

    fix: calc fonc ayrı bir go source'una eklendi

---
+++ learning-git/calculate.go
@@ -0,0 +1,5 @@
+package main
+
+func Calc(a, b float64) float64 {
+	return a + b
+}
--- learning-git/main.go
+++ learning-git/main.go
@@ -3,10 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	result := calc(1, 2)
+	result := Calc(1, 2)
 	fmt.Println(result)
-}
-
-func calc(a, b float64) float64 {
-	return a + b
 }
